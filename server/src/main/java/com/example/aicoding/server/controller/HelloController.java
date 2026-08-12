5480d9a87c62c90fed24f79d27b32338542bcf5b
hao <sh2619978@126.com>
2026-08-12 12:55:49 +0800

    feat: add logging to HelloController

--- server/src/main/java/com/example/aicoding/server/controller/HelloController.java
+++ server/src/main/java/com/example/aicoding/server/controller/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.aicoding.server.controller;
 
 import com.example.aicoding.server.response.ApiResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private static final Logger log = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/hello")
     public ApiResponse<String> hello() {
+        log.info("hello world endpoint called");
         return ApiResponse.success("hello world");
     }
 }
